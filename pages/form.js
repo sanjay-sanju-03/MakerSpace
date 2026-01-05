f8ca096db696a3d2024ea58246c3ad2ffbaa674f
NIVED C K <143318427+Nivedck@users.noreply.github.com>
2026-01-05 10:11:34 +0530

    Change COLLEGE_COORDS to LBS coordinates
    Updated COLLEGE_COORDS to LBS coordinates

--- pages/form.js
+++ pages/form.js
@@ -4,8 +4,8 @@ import Link from 'next/link';
 
 const PURPOSES = [ 'Work', 'Project', 'Maintenance', 'Visit' ];
 const BRANCHES = ['IT', 'CS', 'CE', 'ME', 'EC', 'EE', 'CB', 'AI'];
-//const COLLEGE_COORDS = { lat: 12.5041, lng: 75.0808 }; //LBS Cordinates
-const COLLEGE_COORDS = { lat: 12.419475130348673, lng: 75.18879145754411}; // Kodoth (For testing)
+const COLLEGE_COORDS = { lat: 12.5041, lng: 75.0808 }; //LBS Cordinates
+//const COLLEGE_COORDS = { lat: 12.419475130348673, lng: 75.18879145754411}; // Kodoth (For testing)
 const GEOFENCE_RADIUS_M = 100;
 
 function distanceMeters(a, b) {
